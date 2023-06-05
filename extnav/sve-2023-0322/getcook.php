099507671395a6fe14e38a0c33d362b02e4ef46e
guruni13 <133293217+guruni13@users.noreply.github.com>
2023-06-05 17:49:19 +0900

    Add files via upload

---
+++ extnav/sve-2023-0322/getcook.php
@@ -0,0 +1,10 @@
+<html>
+   
+   <body>
+      <center><h2>Accessing "SameSite:Strict" Cookies</h2>
+      <?php
+         echo "<h3>"."Your Cookie is : " .$_COOKIE["name"] ."</h3>";
+      ?>
+      </center>
+   </body>
+</html>
---
+++ extnav/sve-2023-0322/setcook.php
@@ -0,0 +1,17 @@
+
+<?php
+   setcookie("name", "SHADOW2639", ['SameSite' => 'Strict']);
+?>
+<html>
+   <body>
+      <center><h2>Setting Cookies with "SameSite:Strict"</h2>
+      <br>
+      <h3> Refresh the page once to set the cookie</h3>
+      <?php
+         echo "<h4>"."Cookie Value set to : " .$_COOKIE["name"]. "</h4>";
+         ?>
+         </center>
+   </body>
+   
+</html>
+
